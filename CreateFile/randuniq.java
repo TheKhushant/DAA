81662709ba81f34cf0de11cdee9f42581948a95b
Khushant Wankhede <yashwankhede53@gmail.com>
2024-11-13 19:09:46 +0530

    Update randuniq.java

--- CreateFile/randuniq.java
+++ CreateFile/randuniq.java
@@ -1,33 +1,33 @@
-//program for randam with unique number in file handling
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.HashSet;
-public class sortedno {
+
+public class randomeUniques {
     public static void main(String[] args) {
-        HashSet<Integer> huniq = new HashSet<Integer>();
+        HashSet<Integer> huniq = new HashSet<>();
 
         Scanner sc = new Scanner(System.in);
-        int n,count=0,un;
-        System.out.println("enter the size : ");
+        int n, count = 0, un;
+        System.out.print("Enter the number of unique random numbers to generate: ");
         n = sc.nextInt();
         sc.close();
+
         String fileName = "uniquenum1.txt";
-        try(PrintWriter writer = new PrintWriter(new FileWriter(fileName))){
-            for(int i=1;i<=n || count<=n;i++){
+        
+        try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
+            while (count < n) {  
                 un = (int) Math.floor(Math.random() * 100000) + 1;
-                if(!(huniq.contains(un)))
-                {
-                    huniq.add(un);                      // to add random number in hashset 
-                    writer.println(un + "\n");          //to print random number in file
-                    count++;                            // increase file number
+                if (!huniq.contains(un)) {
+                    huniq.add(un);                     
+                    writer.println(un);                
+                    count++;                           
                 }
             }
-            System.out.println("Numbers from 1 to "+ n +" have been written to " + fileName);
-        }
-        catch(IOException e){
-            System.err.println("An IOException occurred : " + e.getMessage());
+            System.out.println("Unique numbers written to " + fileName);
+        } catch (IOException e) {
+            System.err.println("An IOException occurred: " + e.getMessage());
         }
     }
 }
